5b0e86f99a7bf6f24488e7f030e34e923dd0c37f
Kubanychbekov Bek <kbnv001@gmail.com>
2021-04-14 16:21:39 +0600

    added file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowzerRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowzerRouter>
     <App />
+    </BrowzerRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
